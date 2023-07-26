1214aa6d9fe1ed3208e88f045cf84a26905c2a85
Anoromi <anoromi@DESKTOP-1SVA5E4>
2023-07-26 10:28:43 +0300

    Minor fix stage 2

--- app/themeProvider.tsx
+++ app/themeProvider.tsx
@@ -1,10 +1,11 @@
 import { useAppSelector } from "./utils/clientUseRedux";
-import styles from "@/app/ThemeProvider.module.scss"
+import styles from "@/app/themeProvider.module.scss"
+import classNames from "classnames";
 
 type Props = React.PropsWithChildren<{}>;
 
 export default function ThemeProvider({ children }: Props) {
   const theme = useAppSelector((state) => state.extra.theme);
 
-  return <div className={theme}>{children}</div>;
+  return <div className={classNames(styles.themeProvider, theme)}>{children}</div>;
 }
