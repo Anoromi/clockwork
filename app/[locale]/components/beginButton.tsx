e904bdcc871fd134bd022e8094816c0755c42414
Anoromi <and.zagorulko@gmail.com>
2024-11-14 20:58:40 +0200

    update main page

---
+++ app/[locale]/components/beginButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRipple } from "@/app/components/useRipple";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+import styles from "./beginButton.module.scss"
+import classNames from "classnames";
+
+// type BottomAppBarProps = {
+//   locale: string;
+// };
+
+export function BeginButton({ children }: PropsWithChildren<{}>) {
+  const { buttonData, rippleData } = useRipple();
+  return (
+    <Link href="./timer" 
+    {...buttonData}
+    className={classNames(styles.button, buttonData.className)}>
+      <div {...rippleData}></div>
+      Begin
+    </Link>
+  );
+}
