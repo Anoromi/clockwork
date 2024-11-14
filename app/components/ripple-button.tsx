697e58018749d1999852132a0531572757191bdf
Anoromi <and.zagorulko@gmail.com>
2024-11-14 20:59:10 +0200

    cleanup

--- app/components/ripple-button.tsx
+++ app/components/ripple-button.tsx
@@ -21,56 +21,6 @@ export default function RippleButton({
   type = "button",
   disabled,
 }: React.PropsWithChildren<Props>) {
-  // const [rippleClicked, setRippleClicked] = useState(false)
-  // const [ripplePos, setRipplePos] = useState<{
-  // 	x: number
-  // 	y: number
-  // 	diameter: number
-  // 	startedAt: Date
-  // } | null>(null)
-
-  // const [calming, setCalming] = useState<{
-  // 	diameter: number
-  // } | null>(null)
-
-  // function calculateRipple(e: React.MouseEvent<HTMLElement, MouseEvent>) {
-  // 	setCalming(null)
-  // 	setCalming(ripplePos)
-  // 	const diameter = Math.max(
-  // 		e.currentTarget.clientWidth,
-  // 		e.currentTarget.clientHeight
-  // 	)
-  // 	const radius = diameter / 2
-  // 	setRipplePos({
-  // 		x: e.clientX - (e.currentTarget.offsetLeft + radius),
-  // 		y: e.clientY - (e.currentTarget.offsetTop + radius),
-  // 		diameter,
-  // 		startedAt: new Date(),
-  // 	})
-  // 	// setRippleClicked(true)
-  // }
-  // console.log("hehe", className)
-
-  // const rippleDuration = 300
-  // function finishRipple() {
-  // 	if (ripplePos === null) return
-  // 	const oldRipple = ripplePos
-
-  // 	setTimeout(() => {
-  // 		const ripple = ripplePos
-  // 		if (oldRipple !== ripple) return
-
-  // 		setRipplePos(null)
-  // 		calmDown({ diameter: ripple!.diameter })
-  // 	}, rippleDuration - new Date().getTime() + ripplePos!.startedAt.getTime() - 100)
-  // }
-
-  // function calmDown(params: typeof calming) {
-  // 	setCalming(params)
-  // 	setTimeout(() => {
-  // 		setCalming(null)
-  // 	}, 400)
-  // }
 
   let { buttonData, rippleData } = useRipple({
     disabled,
@@ -84,24 +34,11 @@ export default function RippleButton({
   }
   return (
     <Element
-      // onMouseDown={(e) => calculateRipple(e)}
-      // onMouseUp={(e) => finishRipple()}
-      // onMouseOut={(e) => finishRipple()}
-
       {...buttonData}
       {...extraProps}
-      // onClick={calculateRipple}
-
       className={classNames(buttonData.className, className)}
-      //onPointerDown={(e) => {
-      //    console.log(e)
-      //  }}
     >
       <div
-        // className={classNames(styles.ripple, {
-        // 	[styles["ripple-calm"]]: calming,
-        // })}
-        // style={}
         {...rippleData}
       ></div>
       {children}
