308d9b2aaeacfdfdfca2d3335c7c20aee3598d01
Anoromi <and.zagorulko@gmail.com>
2024-11-20 18:43:08 +0200

    cleanup

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,40 +6,10 @@ import { store } from "./store";
 
 type Props = React.PropsWithChildren<{}>;
 
-//function makeClient() {
-//  const httpLink = new HttpLink({
-//    // https://studio.apollographql.com/public/spacex-l4uc6p/
-//    uri: "/api/graphql",
-//  });
-//
-//  return new NextSSRApolloClient({
-//    cache: new NextSSRInMemoryCache(),
-//    link:
-//      typeof window === "undefined"
-//        ? ApolloLink.from([
-//            new SSRMultipartLink({
-//              stripDefer: true,
-//            }),
-//            httpLink,
-//          ])
-//        : httpLink,
-//  });
-//}
-//
-//function makeSuspenseCache() {
-//  return new SuspenseCache();
-//}
-
 export default function Providers({ children }: Props) {
   return (
     <Provider store={store}>
       {children}
-      {
-        //<ApolloNextAppProvider
-        //  makeClient={makeClient}
-        //  makeSuspenseCache={makeSuspenseCache}
-        //></ApolloNextAppProvider>
-      }
     </Provider>
   );
 }
