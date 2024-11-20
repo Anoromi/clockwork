b0fb529761cd396ead7a5a29d5103f4172df1f21
Anoromi <and.zagorulko@gmail.com>
2024-11-20 18:44:59 +0200

    fix options menu

--- app/[locale]/library/components/optionsMenu.tsx
+++ app/[locale]/library/components/optionsMenu.tsx
@@ -2,28 +2,24 @@
 import styles from "@/app/[locale]/library/components/optionsMenu.module.scss";
 import CommonButton from "@/app/components/button/commonButton";
 import RippleButton from "@/app/components/ripple-button";
-import utilStyles from "@/app/styles/utils.module.scss";
 import { useAppDispatch } from "@/app/utils/clientUseRedux";
-import { Menu } from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { Icon } from "@iconify/react";
 import { changeAddDialog } from "../libraryStore";
 
-type Props = {
-};
+type Props = {};
 
 export default function OptionsMenu({}: Props) {
   const dispatch = useAppDispatch();
   return (
     <>
       <Menu as="div" className={styles.menuWrapper}>
-        <Menu.Button className={utilStyles.contents}>
-          <CommonButton buttonType={"blank"} textType={"icon"} as="div">
-            <Icon icon="material-symbols:more-vert" fontSize={24} />
-          </CommonButton>
-        </Menu.Button>
-        <Menu.Items className={styles.menuItems}>
-          <Menu.Item>
-            {({ active }) => (
+        <MenuButton as={CommonButton} buttonType="blank" textType="icon">
+          <Icon icon="material-symbols:more-vert" fontSize={24} />
+        </MenuButton>
+        {
+          <MenuItems className={styles.menuItems}>
+            <MenuItem>
               <RippleButton
                 as="button"
                 onClick={() => {
@@ -33,22 +29,22 @@ export default function OptionsMenu({}: Props) {
               >
                 Add activity
               </RippleButton>
-            )}
-          </Menu.Item>
-          {
-            // <Menu.Item>
-            //   {({ active }) => (
-            //     <RippleButton
-            //       as="button"
-            //       onClick={() => {}}
-            //       className={styles.menuItem}
-            //     >
-            //       Options
-            //     </RippleButton>
-            //   )}
-            // </Menu.Item>
-          }
-        </Menu.Items>
+            </MenuItem>
+            {
+              // <Menu.Item>
+              //   {({ active }) => (
+              //     <RippleButton
+              //       as="button"
+              //       onClick={() => {}}
+              //       className={styles.menuItem}
+              //     >
+              //       Options
+              //     </RippleButton>
+              //   )}
+              // </Menu.Item>
+            }
+          </MenuItems>
+        }
       </Menu>
     </>
   );
