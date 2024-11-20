d92ebcdd309913bf380719c6165593de7d012498
Anoromi <and.zagorulko@gmail.com>
2024-11-20 18:42:58 +0200

    silence sass deprecation warning

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const createNextIntlPlugin = require('next-intl/plugin');
+const createNextIntlPlugin = require("next-intl/plugin");
 
-const withNextIntl = createNextIntlPlugin('./app/request.ts');
+const withNextIntl = createNextIntlPlugin("./app/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withNextIntl({
   webpack: {},
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
 });
 
 module.exports = withNextIntl(nextConfig);
